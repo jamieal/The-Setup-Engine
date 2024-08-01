c2d70a12f30fa9130083a223f263c81596274cfb
Jamie Cras <jamiecras@gmail.com>
2024-08-01 17:08:54 +0100

    Update jamie2.sh

--- jamie2.sh
+++ jamie2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running install script"
+echo "Welcome to the installer"
 #check homebrew homebrew https://brew.sh/
 which -s brew
 if [[ $? != 0 ]] ; then
@@ -13,11 +13,11 @@ fi
 
 wait 
 echo "homebrew command ran"
-#if m1....
-m1="checking to see if m1"
+#check chip architecture
+cpuArc="checking to see if Silicone"
 if [[ $(uname -p) == 'arm' ]]; then
-  m1=$(uname -p)
-  echo "Detected $m1 processor, making change to path for brew"
+  cpuArc=$(uname -p)
+  echo "Detected $cpuArc processor, making change to path for brew"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/jcras/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
@@ -26,27 +26,9 @@ echo "Brew installed - ready to install applications"
 #check to see if these are installed 
 #change dock and put on right/hide
 #array the apps
-#osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Downloads/cat.jpg"'
-
-
-#check for app
-#if [ -d "/Applications/zoom.us.app" ] ; then
-#	zoomapp="Installed"
-#	if [ -f "/Applications/zoom.us.app/Contents/Info.plist" ] ; then
-#		zoomversion=$(defaults read "/Applications/zoom.us.app/Contents/Info.plist" CFBundleVersion)
-#		#zoomversion=$(defaults read "/Applications/zoom.us.app/Contents/Info.plist" CFBundleShortVersionString)
-#	else
-#		zoomversion="Not Installed"
-#	fi  
-#else
-#	ZOOM_INSTALL_VALIDATION_ERRORS+=("zoomapp: application not found")
-#	zoomapp="Not Installed"
-#fi
-
 
 #declare array
-#declare -a applications=(Spotify Discord Franz Rectangle visual-studio-code VLC microsoft-excel)
-declare -a applications=(visual-studio-code)
+declare -a applications=(visual-studio-code Spotify Discord Rectamble)
 
 for i in "${applications[@]}"
   do  
@@ -55,11 +37,8 @@ for i in "${applications[@]}"
 done
 
 #dock stuff
-
 defaults write com.apple.dock single-app -bool TRUE
 defaults write com.apple.dock orentation right
 killall Dock
-
-
 
 exit 0

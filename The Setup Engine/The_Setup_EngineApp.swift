01eaef604f27bea08b7ab7065e6e6a74c817b1cb
Jamie Cras <jamiecras@gmail.com>
2026-03-09 18:59:51 -0400

    Initial Commit

---
+++ The Setup Engine/The_Setup_EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Setup_EngineApp.swift
+//  The Setup Engine
+//
+//  Created by Jamie Cras on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct The_Setup_EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
